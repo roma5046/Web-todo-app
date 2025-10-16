b8faca72ed619d73697c1be0838a413bc307474f
roma5046 <49899746+roma5046@users.noreply.github.com>
2025-10-16 12:52:22 +0800

    Create app.py
    Создан первый файл с кодом Flask

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask # Импорт Flask
+
+app = Flask(__name__) #Создание экземпляра приложения
+# Определение маршрута (адрес) для главной страницы
+@app.route('/')
+def home():
+    return "hello word! Этот проект будет использовать фреймворк Flask.
+# Запуск сервера, если файл запущен
+if __name__ == '__main__':
+    app.run(debug=True)
