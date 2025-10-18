b831113d2870a6516d427cbad07f9df836edf588
roma5046 <roma5046@ya.ru>
2025-10-18 18:44:37 +0800

    Добавлена форма To-Do List(index.html) и обновлён app.py

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask # Импорт Flask
+from flask import Flask, render_template, request, redirect, url_for # Импорт необходимых функций из Flask
 
 app = Flask(__name__) #Создание экземпляра приложения
-# Определение маршрута (адрес) для главной страницы
-@app.route('/')
+
+tasks = [] # Список задач (в памяти)
+
+@app.route('/') # Маршрут для главной страницы
 def home():
-    return "hello word! Этот проект будет использовать фреймворк Flask."
+    return render_template('index.html', tasks=tasks) # указание HTML-шаблона и передача списока задач
+
+@app.route('/add', methods=['POST'])# Маршрут добавления новой задачи, только POST-запросы
+def add_task():
+    title = request.form.get('title') # Получение названия задачи из формы
+    description = request.form.get('description') # Получение описания задачи из формы
+    tasks.append({'title':title, 'description': description}) # Добавление задачи в список
+    return redirect(url_for('home')) # Перенаправление на главную страницу
+
 # Запуск сервера, если файл запущен
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '__main__': # Запуск сервера, если файл запущен
+    app.run(debug=True) # Запуск серверa в режиме отладки
